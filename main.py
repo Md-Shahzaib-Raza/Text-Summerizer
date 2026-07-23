8fac575b4f04ab8ada271b5252fdf7bd9c6893db
Md-Shahzaib-Raza <shahzaib.raza.1969@gmail.com>
2026-07-23 20:18:36 +0000

    Fixed Model Trainer Bug

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import unittest
+unittest.TestCase.assertRaisesRegexp = unittest.TestCase.assertRaisesRegex
 from TextSummerizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummerizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from TextSummerizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
